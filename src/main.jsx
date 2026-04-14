367a83ca8bfe6366541cf57ded819300bd8d0df3
SamiGaming <104452588+SamiGamin@users.noreply.github.com>
2026-04-14 01:47:59 +0000

    "Activando el Service Worker para instalacion PWA"

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css' // Importamos los estilos globales y Tailwind
 
+// 1. IMPORTAMOS EL REGISTRO DEL PWA
+import { registerSW } from 'virtual:pwa-register'
+
+// 2. LO ENCENDEMOS AUTOMÁTICAMENTE
+registerSW({ immediate: true })
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
